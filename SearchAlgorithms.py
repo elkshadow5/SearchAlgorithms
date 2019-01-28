7b95ece3fa495323fd8d2218b11f8624a835e64d
David Jefts <dvdjefts27@gmail.com>
2019-01-27 22:29:13 -0500

    Set up possibility for undirected graph with connected nodes. blegh

--- SearchAlgorithms.py
+++ SearchAlgorithms.py
@@ -1,33 +1,71 @@
-class Search():
+class GridPoint:
+    def __init__(self, row, col, let):
+        self.row = row
+        self.col = col
+        self.let = let
+        self.neighbors = []
     
+    def addNeighbors(self, neighbor):
+        self.neighbors.append(neighbor)
+    
+    def set_let(self, let):
+        self.let = let
+
+
+class Search:
     def __init__(self):
-        self.arena = [['O' for col in range(7)] for row in range(6)]
+        self.arena = [[GridPoint(row, col, 'O') for col in range(7)] for row in range(6)]
+        # GRID INITIALIZATION
         #         [r  c]
-        self.arena[5][0] = 'S'  # start
-        self.arena[5][6] = 'G'  # goal
-        self.arena[0][0] = 'X'
-        self.arena[0][1] = 'X'
-        self.arena[0][3] = 'X'
-        self.arena[0][4] = 'X'
-        self.arena[0][5] = 'X'
-        self.arena[0][6] = 'X'
-        self.arena[1][0] = 'X'
-        self.arena[1][6] = 'X'
-        self.arena[2][0] = 'X'
-        self.arena[2][2] = 'X'
-        self.arena[2][3] = 'X'
-        self.arena[3][3] = 'X'
-        self.arena[3][5] = 'X'
-        self.arena[4][1] = 'X'
-        self.arena[4][3] = 'X'
-        self.arena[4][5] = 'X'
-        self.arena[5][1] = 'X'
-        self.arena[5][5] = 'X'
+        self.arena[5][0].set_let('S')  # START
+        self.arena[5][6].set_let('G')  # GOAL
+        self.arena[0][0].set_let('X')
+        self.arena[0][1].set_let('X')
+        self.arena[0][3].set_let('X')
+        self.arena[0][4].set_let('X')
+        self.arena[0][5].set_let('X')
+        self.arena[0][6].set_let('X')
+        self.arena[1][0].set_let('X')
+        self.arena[1][6].set_let('X')
+        self.arena[2][0].set_let('X')
+        self.arena[2][2].set_let('X')
+        self.arena[2][3].set_let('X')
+        self.arena[3][3].set_let('X')
+        self.arena[3][5].set_let('X')
+        self.arena[4][1].set_let('X')
+        self.arena[4][3].set_let('X')
+        self.arena[4][5].set_let('X')
+        self.arena[5][1].set_let('X')
+        self.arena[5][5].set_let('X')
+        # GRAPH INITIALIZATION
+        self.arena[5][0].addNeighbors((self.arena[4][0],))  # START
+        self.arena[4][0].addNeighbors((self.arena[5][0], self.arena[3][0],))
+        self.arena[3][0].addNeighbors((self.arena[4][0], self.arena[3][1],))
+        self.arena[3][1].addNeighbors((self.arena[3][0], self.arena[3][2], self.arena[2][1]))
+        self.arena[2][1].addNeighbors((self.arena[3][1], self.arena[1][1],))
+        self.arena[1][1].addNeighbors((self.arena[2][1], self.arena[1][2],))
+        self.arena[5][2].addNeighbors((self.arena[4][2], self.arena[5][3],))
+        self.arena[4][2].addNeighbors((self.arena[5][2], self.arena[3][2],))
+        self.arena[3][2].addNeighbors((self.arena[4][2], self.arena[3][1],))
+        self.arena[1][2].addNeighbors((self.arena[1][1], self.arena[0][2], self.arena[1][3],))
+        self.arena[0][2].addNeighbors((self.arena[1][2],))
+        self.arena[5][3].addNeighbors((self.arena[5][2], self.arena[5][4],))
+        self.arena[1][3].addNeighbors((self.arena[1][2], self.arena[1][4],))
+        self.arena[5][4].addNeighbors((self.arena[5][3], self.arena[4][4],))
+        self.arena[4][4].addNeighbors((self.arena[5][4], self.arena[3][4],))
+        self.arena[3][4].addNeighbors((self.arena[4][4], self.arena[2][4],))
+        self.arena[2][4].addNeighbors((self.arena[3][4], self.arena[1][4], self.arena[2][5],))
+        self.arena[1][4].addNeighbors((self.arena[1][3], self.arena[1][5], self.arena[2][4],))
+        self.arena[2][5].addNeighbors((self.arena[2][4], self.arena[2][6], self.arena[1][5],))
+        self.arena[1][5].addNeighbors((self.arena[1][4], self.arena[2][5],))
+        self.arena[5][6].addNeighbors((self.arena[4][6],))  # GOAL
+        self.arena[4][6].addNeighbors((self.arena[3][6],))
+        self.arena[3][6].addNeighbors((self.arena[2][6],))
+        self.arena[2][6].addNeighbors((self.arena[2][5], self.arena[3][6],))
         
-        row, col = 0, 0
-        self.currSpot = [row, col]
+        self.currSpot = self.arena[5][0]
     
-    ### SEARCH ALGORITHMS ###
+    # SEARCH ALGORITHMS #
     def depth(self):
         # TODO: Depth-first Search algorithm
         while self.getSpot() != 'G':
@@ -42,34 +80,34 @@ class Search():
         # TODO: A* search algorithm
         return 0
     
-    ### MOVEMENT ###
+    # MOVEMENT #
     def up(self):
-        self.currSpot[0] -= 1
-        if self.getSpot() == 'X':
-            self.currSpot[0] -= 1
+        if self.arena[self.currSpot.row - 1][self.currSpot.col].let == 'X' and 'P':
             return False
-        self.arena[self.currSpot[0]][self.currSpot[1]] = 'P'
+        self.currSpot = self.arena[self.currSpot.row - 1][self.currSpot.col]
+        self.arena[self.currSpot.row][self.currSpot.col].let = 'P'
+        return True
     
     def down(self):
-        self.currSpot[0] += 1
-        if self.getSpot() == 'X':
-            self.currSpot[0] += 1
+        if self.arena[self.currSpot.row + 1][self.currSpot.col].let == 'X' and 'P':
             return False
-        self.arena[self.currSpot[0]][self.currSpot[1]] = 'P'
+        self.currSpot = self.arena[self.currSpot.row + 1][self.currSpot.col]
+        self.arena[self.currSpot.row][self.currSpot.col].let = 'P'
+        return True
     
     def left(self):
-        self.currSpot[1] -= 1
-        if self.getSpot() == 'X':
-            self.currSpot[1] -= 1
+        if self.arena[self.currSpot.row][self.currSpot.col - 1].let == 'X' and 'P':
             return False
-        self.arena[self.currSpot[0]][self.currSpot[1]] = 'P'
+        self.currSpot = self.arena[self.currSpot.row][self.currSpot.col - 1]
+        self.arena[self.currSpot.row][self.currSpot.col].let = 'P'
+        return True
     
     def right(self):
-        self.currSpot[1] += 1
-        if self.getSpot() == 'X':
-            self.currSpot[1] += 1
+        if self.arena[self.currSpot.row][self.currSpot.col + 1].let == 'X' and 'P':
             return False
-        self.arena[self.currSpot[0]][self.currSpot[1]] = 'P'
+        self.currSpot = self.arena[self.currSpot.row][self.currSpot.col + 1]
+        self.arena[self.currSpot.row][self.currSpot.col].let = 'P'
+        return True
     
     def getSpot(self):
         return self.arena[self.currSpot[0]][self.currSpot[1]]
@@ -77,8 +115,8 @@ class Search():
     def printState(self):
         for row in ([0, 1, 2, 3, 4, 5]):
             for col in ([0, 1, 2, 3, 4, 5, 6]):
-                print(self.arena[row][col], end = '  ')
-            print()
+                print(self.arena[row][col].let, end = '  ')
+            print() # new line
 
 
 search = Search()
@@ -86,4 +124,16 @@ search.printState()
 print()
 search.up()
 search.up()
+search.right()
+search.up()
+search.up()
+search.right()
+search.right()
+search.right()
+search.right()
+search.down()
+search.right()
+search.down()
+search.down()
+search.down()
 search.printState()

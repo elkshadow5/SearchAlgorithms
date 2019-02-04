08ecbaa049c2a145dafde254eab91d3afbb18cb7
atforste <atforste@gmail.com>
2019-02-03 20:50:08 -0500

    Fixed neighbors not being objects

--- SearchAlgorithms.py
+++ SearchAlgorithms.py
@@ -1,3 +1,5 @@
+import copy
+
 class GridPoint:
     def __init__(self, let, color):
         self.let = let
@@ -6,9 +8,19 @@ class GridPoint:
     
     def addNeighbors(self, neighbor):
         self.neighbors.append(neighbor)
+
+    def hasConflict(self):
+        for neighbor in self.neighbors:
+            if neighbor.color==self.color:
+                return True
+        return False
     
     def __repr__(self):
-        return self.let + " --- " + "[" + ', '.join(self.neighbors) + "]"
+        returnString = self.let+" --- ["
+        for neighbor in self.neighbors:
+            returnString = returnString+neighbor.let+" "
+        returnString+="]"
+        return returnString
 
 
 class Search:
@@ -26,19 +38,46 @@ class Search:
         for grdpt in self.grid:
             for adj in self.map:
                 if grdpt.let == adj[0]:
-                    grdpt.addNeighbors(adj[1])
+                    for neighbor in self.grid:
+                        if neighbor.let==adj[1]:
+                            grdpt.addNeighbors(neighbor)
                 elif grdpt.let == adj[1]:
-                    grdpt.addNeighbors(adj[0])
+                    for neighbor in self.grid:
+                        if neighbor.let==adj[0]:
+                            grdpt.addNeighbors(neighbor)
     
     # SEARCH ALGORITHMS #
     def breadth(self):
+        #USE THIS GRID! Keeps from messing up the later searches
+        grid = copy.deepcopy(self.grid)
         return 0
     
     def depth(self):
-        return 0
+        grid = copy.deepcopy(self.grid)
+        stack = []
+        closed = []
+        stack.append(grid[0])
+        while len(stack)>0:
+            point = stack.pop()
+            closed.append(point)
+            for neighbor in point.neighbors:
+                if neighbor not in closed and neighbor not in stack:
+                    stack.append(neighbor)
+            colorToTry = 0
+            point.color = self.colors[colorToTry]
+            while point.hasConflict():
+                print(point.color)
+                colorToTry=colorToTry+1
+                point.color = self.colors[colorToTry]
+
+        print("Depth first search result:")
+        for point in grid:
+            print(str(point)+" color: "+point.color)  
     
     def idfs(self):
+        grid = copy.deepcopy(self.grid)
         return 0
+
     
     def printState(self):
         for pt in self.grid:
@@ -48,3 +87,4 @@ class Search:
 
 search = Search()
 search.printState()
+search.depth()

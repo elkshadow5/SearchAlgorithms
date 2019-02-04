95db4ad2f12599c48fc1c394529c8c1ef6e2ceb9
atforste <atforste@gmail.com>
2019-02-03 19:55:22 -0500

    Added colors to gridpoints and main search class

--- SearchAlgorithms.py
+++ SearchAlgorithms.py
@@ -1,6 +1,7 @@
 class GridPoint:
-    def __init__(self, let):
+    def __init__(self, let, color):
         self.let = let
+        self.color = color
         self.neighbors = []
     
     def addNeighbors(self, neighbor):
@@ -12,6 +13,8 @@ class GridPoint:
 
 class Search:
     def __init__(self):
+        #basic list of colors to try. Will only work if enough colors fro problem
+        self.colors = ["red", "green","blue","cyan","yellow","magenta","black"]
         self.map = [("r1", "r2"), ("r1", "r3"), ("r2", "r4"), ("r3", "r4")]
         self.grid = []
         added = []
@@ -19,7 +22,7 @@ class Search:
             for grdpt in adj:
                 if grdpt not in added:
                     added.append(grdpt)
-                    self.grid.append(GridPoint(grdpt))
+                    self.grid.append(GridPoint(grdpt,"black"))
         for grdpt in self.grid:
             for adj in self.map:
                 if grdpt.let == adj[0]:
@@ -29,15 +32,12 @@ class Search:
     
     # SEARCH ALGORITHMS #
     def breadth(self):
-        # TODO: Depth-first Search algorithm
         return 0
     
     def depth(self):
-        # TODO: Greedy best-first search algorithm
         return 0
     
     def idfs(self):
-        # TODO: A* search algorithm
         return 0
     
     def printState(self):

3fedf4a15551bedbeea917a7dade4ca51de209cb
atforste <atforste@gmail.com>
2019-02-04 14:40:21 -0500

    Finshed DFS and IDFS. Chaned how search gets the map. Added method to print end state

--- SearchAlgorithms.py
+++ SearchAlgorithms.py
@@ -26,10 +26,10 @@ class GridPoint:
 
 
 class Search:
-    def __init__(self):
+    def __init__(self, map):
         #basic list of colors to try. Will only work if enough colors for problem
         self.colors = ["red", "green","blue","cyan","yellow","magenta"]
-        self.map = [("r1", "r2"), ("r1", "r3"), ("r2", "r4"), ("r3", "r4")]
+        self.map = map
         self.grid = []
         added = []
         #Create GridPoint Objects
@@ -66,6 +66,9 @@ class Search:
 
         #Start going through the stack        
         while not self.reachedEndCondition(grid):
+            #if stack is empty, failed
+            if len(stack)==0:
+                break
             point = stack.pop()
             closed.append(point)
             print("\tNow working with node "+point.let+"...")
@@ -84,12 +87,64 @@ class Search:
                 point.color = self.colors[colorToTry]
 
         print("Depth first search result:")
-        for point in grid:
-            print("\t"+point.let+" color: "+point.color)  
+        #Print the solution, if found
+        self.printSolution(grid) 
     
-    def idfs(self):
+    def idfs(self, minDepth, maxDepth):
+        print("\nIterative Depth first searching: ")
+        #Setup. Perform deep copy to preserve main grid
         grid = copy.deepcopy(self.grid)
-        return 0
+
+        #Iterate through the various depths
+        for depth in range(minDepth,maxDepth) :
+            #Before resetting, check to see if solution was found on last iteration
+            if self.reachedEndCondition(grid):
+                break
+            
+            #Reset for new depth
+            grid = copy.deepcopy(self.grid)
+            stack = []
+            closed = []
+            stack.append(grid[0])
+            currentDepth = 0
+            print("\tNow working up to a depth of "+str(depth)+":")
+
+            #Start going through the stack        
+            while not self.reachedEndCondition(grid):
+                #if stack is empty, failed
+                if len(stack)==0:
+                    break
+                point = stack.pop()
+                closed.append(point)
+                print("\t\tNow working with node "+point.let+"...")
+                #Add neighbors to the stack, assuming they haven't been
+                #  visited, are already there, or depth limit reached
+                if currentDepth<depth:
+                    for neighbor in point.neighbors:
+                        if neighbor not in closed and neighbor not in stack:
+                            stack.append(neighbor)
+                currentDepth+=1
+                
+                #Go through the list of colors until get one so its different
+                #  than any of its neighbors' colors
+                colorToTry = 0
+                point.color = self.colors[colorToTry]
+                while point.hasConflict():
+                    colorToTry+=1
+                    point.color = self.colors[colorToTry]
+
+        print("Iterative Depth first search result:")
+        #Print solution, if found
+        self.printSolution(grid)
+
+    #Print out solution, if found
+    def printSolution(self,grid):
+        resultFound = "!!!NO SOLUTION FOUND!!!"
+        if self.reachedEndCondition(grid):
+            resultFound = "Solution found:"
+        print("\t"+resultFound)
+        for point in grid:
+            print("\t"+point.let+" color: "+point.color)
 
     #Check to see if every point has a color and there are no conflicts
     def reachedEndCondition(self, gridIn):
@@ -103,7 +158,14 @@ class Search:
         for pt in self.grid:
             print(pt)
 
-
-search = Search()
+#Put map here:
+    #BASIC TEST
+search = Search([("r1", "r2"), ("r1", "r3"), ("r2", "r4"), ("r3", "r4")])
+    #ADVANCED TEST
+#search = Search([("r1", "r2"), ("r1", "r3"), ("r2", "r4"), ("r3", "r4"),("r3", "r5"),("r5", "r4"),("r5", "r6")])
+#Print out the map
 search.printState()
+#Show the depth first searching
 search.depth()
+#Show IDFS. First number is minimum depth, second is maximum depth
+search.idfs(1,3)
